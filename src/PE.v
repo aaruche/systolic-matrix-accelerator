5029062891cebe857985dbc572004864ab3e9640
aaruche <aarusharya88@gmail.com>
2026-09-12 02:12:49 +0300

    - removed A/B_valid_forw (F2)  as pe_valid des their job otherwise having them is just redundancy
    - Operand forwarding logic is in progress

--- src/PE.v
+++ src/PE.v
@@ -16,17 +16,16 @@ module PE (
     input signed [7:0] B ,  
     output signed [31:0] C ,
 
-    input A_valid_forw,                      // whether the current A input contains valid matrix data (F3)
-    input B_valid_forw,                      // whether the current B input contains valid matrix data (F3)
+    
 
     input accum_clr,                         // begin a new matrix multiplication / clear the previous C result 
 
-    output signed [7:0] A_forw_op,           // corresponding 8 bit forwarding output for forw_reg (F1) 
+    output signed [7:0] A_forw_op,           // corresponding 8 bit forwarding output for A/B_forw_reg (F1) 
     output signed [7:0] B_forw_op
 
 );
  reg signed [15:0] mult_reg ; 
- reg mult_valid_reg;                         // whether the product stored in mult_reg is valid and should be accumulated (F2)
+ reg mult_valid_reg;                         // whether the product stored in mult_reg is valid and should be accumulated (F3)
  
  reg signed [31:0] accum_reg ; 
  
@@ -35,6 +34,29 @@ module PE (
  reg signed[7:0] B_forw_reg ;
 
                    
+always @(posedge clk) begin
+    if (!rst_n) begin 
+        mult_reg    <= 16'b0;
+        accum_reg   <= 32'b0; 
+        A_forw_reg  <= 8'b0;
+        B_forw_reg  <= 8'b0;
+
+        mult_valid_reg <= 1'b0; 
+        A_valid_forw   <= 1'b0;
+        B_valid_forw   <= 1'b0;
+    end 
+
+// Operands Forwarding 
+always @(posedge clk) begin 
+    if (pe_valid && !accum_clr) begin 
+        A_forw_reg <= A; 
+        B_forw_reg <= B; 
+    end 
+end 
+assign A_forw_op = A_forw_reg ;
+assign B_forw_op = B_forw_reg ;
+
+end 
 
 
 
