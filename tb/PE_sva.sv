4334a809f3596dd96b8be26f2a43971ec3f76e65
aaruche <aarusharya88@gmail.com>
2026-09-14 01:21:26 +0300

    Created Product Pip SVA - reset property - accum clear property - Capturing property ( Ax B event) - Bubble Case ( bubble == !pe_valid for now - might change bubble condition in future)

--- tb/PE_sva.sv
+++ tb/PE_sva.sv
@@ -29,3 +29,48 @@ endproperty
 
 a_forwarding: assert property (forwarding_property)
     else $error("Operand forwarding property failed");
+
+// Product properties 
+// Product clear 
+property product_clear_property;
+    @(posedge clk)
+    disable iff (!rst_n)
+    accum_clr
+    |=> (
+        dut.mult_reg       == 16'b0 &&
+        dut.mult_valid_reg == 1'b0
+    );
+endproperty
+
+a_product_clear: assert property (product_clear_property)
+    else $error("Product pipeline was not cleared");
+
+// Product Capture 
+property product_capture_property;
+    @(posedge clk)
+    disable iff (!rst_n)
+    (pe_valid && !accum_clr)|=>
+     (
+        dut.mult_reg == ($past(A) * $past(B)) &&
+        dut.mult_valid_reg == 1'b1
+    );
+endproperty
+
+a_product_capture: assert property (product_capture_property)
+    else $error("Product capture failed");
+
+// Product dealing with Bubble (pe_valid = 0)
+property product_bubble_property;
+    @(posedge clk)
+    disable iff (!rst_n)
+    (!pe_valid && !accum_clr)
+    |=> (
+        dut.mult_valid_reg == 1'b0
+    );
+endproperty
+
+a_product_bubble: assert property (product_bubble_property)
+    else $error("Product-valid was not cleared after a bubble");
+
+
+endmodule 
